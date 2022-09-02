277eafcc320a796a5b4b27e07df9874d3ab1804d
nick3842 <nick3842@protonmail.com>
2022-09-02 17:21:25 -0500

    Add fugitive, fix timeout error with surround and comments and add phpdoc ignore to fix m1 specific error better

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -17,7 +17,7 @@ local options = {
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
   -- termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
@@ -25,15 +25,18 @@ local options = {
   shiftwidth = 2,                          -- the number of spaces inserted for each indentation
   tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = false,                  -- set relative numbered lines
+  number = false,                           -- set numbered lines
+  relativenumber = true,                  -- set relative numbered lines
+  nu = true,
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  foldlevel=20,
 }
+
 
 vim.opt.shortmess:append "c"
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,7 +45,8 @@ return packer.startup(function(use)
 	use({ "wbthomason/packer.nvim", commit = "00ec5adef58c5ff9a07f11f45903b9dbbaa1b422" }) -- Have packer manage itself
 	use({ "nvim-lua/plenary.nvim", commit = "968a4b9afec0c633bc369662e78f8c5db0eba249" }) -- Useful lua functions used by lots of plugins
 	use({ "windwp/nvim-autopairs", commit = "fa6876f832ea1b71801c4e481d8feca9a36215ec" }) -- Autopairs, integrates with both cmp and treesitter
-	use({ "numToStr/Comment.nvim", commit = "2c26a00f32b190390b664e56e32fd5347613b9e2" })
+	use({ "numToStr/Comment.nvim" })
+	-- use({ "terrortylor/nvim-comment", commit = "2c26a00f32b190390b664e56e32fd5347613b9e2" })
 	use({ "JoosepAlviste/nvim-ts-context-commentstring", commit = "88343753dbe81c227a1c1fd2c8d764afb8d36269" })
 	use({ "kyazdani42/nvim-web-devicons", commit = "8d2c5337f0a2d0a17de8e751876eeb192b32310e" })
 	-- use({ "kyazdani42/nvim-tree.lua", commit = "bdb6d4a25410da35bbf7ce0dbdaa8d60432bc243" })
@@ -57,6 +58,12 @@ return packer.startup(function(use)
 	use({ "lewis6991/impatient.nvim", commit = "969f2c5c90457612c09cf2a13fee1adaa986d350" })
 	use({ "lukas-reineke/indent-blankline.nvim", commit = "6177a59552e35dfb69e1493fd68194e673dc3ee2" })
 	use({ "goolord/alpha-nvim", commit = "ef27a59e5b4d7b1c2fe1950da3fe5b1c5f3b4c94" })
+	use({ "kylechui/nvim-surround" ,
+    tag = "*",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  })
 	-- use("folke/which-key.nvim")
 
 	-- Colorschemes
@@ -92,13 +99,13 @@ return packer.startup(function(use)
 
   -- Regular VIM plugins
 	use({ "tpope/vim-vinegar" })
-	use({ "tpope/vim-surround" })
+	use({ "tpope/vim-fugitive" })
 	use({ "markonm/traces.vim" })
   use({ "numToStr/Navigator.nvim" })
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
-		require("packer").sync()
+    require("packer").sync()
 	end
 end)
