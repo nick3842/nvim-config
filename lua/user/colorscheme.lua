02254b073d65f0df1fd58f5106b8c45b7f395ca3
nick3842 <nick3842@protonmail.com>
2022-09-05 17:22:58 -0500

    Update colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,9 @@
 vim.cmd [[
 try
+  let g:everforest_background = 'soft'
+  let g:everforest_better_performance = 1
+  let g:everforest_transparent_background = 0
+  let g:everforest_ui_contrast = 'high'
   colorscheme everforest
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
