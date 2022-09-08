6870e04cc44cec6add901ce55a04e5d665734f68
nick3842 <nick3842@protonmail.com>
2022-09-07 20:05:11 -0500

    Remove more annoying keymaps, add lsps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -65,10 +65,10 @@ keymap("v", ">", ">gv", opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+--[[ keymap("x", "J", ":move '>+1<CR>gv-gv", opts) ]]
+--[[ keymap("x", "K", ":move '<-2<CR>gv-gv", opts) ]]
+--[[ keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts) ]]
+--[[ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts) ]]
 
 -- Terminal --
 -- Better terminal navigation
