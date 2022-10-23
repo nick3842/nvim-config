4e35086c1fb17fa9791bb13463116ca41c472a48
nick3842 <nick3842@protonmail.com>
2022-10-23 11:37:20 -0500

    Add transparency

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,7 +2,7 @@ vim.cmd [[
 try
   let g:everforest_background = 'soft'
   let g:everforest_better_performance = 1
-  let g:everforest_transparent_background = 0
+  let g:everforest_transparent_background = 1
   let g:everforest_ui_contrast = 'high'
   colorscheme everforest
 catch /^Vim\%((\a\+)\)\=:E185/
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,6 +70,7 @@ return packer.startup(function(use)
 	use({ "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" })
 	use("lunarvim/darkplus.nvim")
 	use("sainnhe/everforest")
+	use("xiyaowong/nvim-transparent")
 
 	-- cmp plugins
 	use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
