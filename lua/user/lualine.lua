b6bb67497a71cb2d0fc858ed2ad31c52fd5356ba
nick3842 <nick3842@protonmail.com>
2023-01-03 18:54:03 -0600

    update lualine, add marker for 80 chars, add alternate colorscheme

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -38,31 +38,32 @@ local branch = {
   color={fg='#a7c080'}
 }
 
-local location = {
-  "location",
-  padding = 0,
-}
-
-local spaces = function()
-  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-end
+-- local location = {
+--   "location",
+--   padding = 0,
+-- }
+--
+-- local spaces = function()
+--   return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+-- end
 
 lualine.setup {
   options = {
     globalstatus = true,
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = "|", right = "" },
+
+    component_separators = { left = "▏", right = "" },
     section_separators = { left = "", right = "" },
     disabled_filetypes = { "alpha", "dashboard" },
     always_divide_middle = true,
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { branch, diagnostics},
+    lualine_b = { branch },
     lualine_c = { filename },
-    lualine_x = { diff, spaces, "encoding", filetype },
-    lualine_y = { location },
-    lualine_z = { "progress" },
+    lualine_x = { diff, filetype },
+    lualine_y = { diagnostics },
+    lualine_z = { },
   },
 }
