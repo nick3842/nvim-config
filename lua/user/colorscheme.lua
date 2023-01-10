b9f1bcbae3f73ee92035a00f8c43a6642fcd2755
nick3842 <nick3842@protonmail.com>
2023-01-10 17:26:49 -0600

    remove transparent background

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -8,7 +8,6 @@ vim.cmd [[
 try
   let g:everforest_background = 'medium'
   let g:everforest_better_performance = 1
-  let g:everforest_transparent_background = 1
   let g:everforest_diagnostic_virtual_text = "colored"
   let g:everforest_ui_contrast = 'high'
   colorscheme everforest
