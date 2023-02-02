7a25cd0fbd705cbd355cf4e28c392e7de6615c69
nick3842 <nick3842@protonmail.com>
2023-02-02 17:47:26 -0600

    make searching way better

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -86,6 +86,9 @@ keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
 keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
 
+-- searching
+keymap("n", "n", "n:silent! norm zO<cr>zz", opts)
+
 -- Lsp
 keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
 
